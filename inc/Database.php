7d56db3dda739009ce7b32a96e261223f49e949c
erik.masny <erik.masny@gmail.com>
2021-03-18 09:21:35 +0100

    Added connecting to DB in Database.php

--- inc/Database.php
+++ inc/Database.php
@@ -1,5 +1,24 @@
 <?php
 
 class Database {
-    
+    private $host = 'localhost';
+    private $name = 'zadanie3';
+    private $username = 'root';
+    private $password = '';
+
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->name, $this->username, $this->password);
+            $this->connection->exec("set names utf8");
+            echo 'success connected';
+        } catch( PDOException $exception ) {
+            echo "Database could not be connected: " . $exception->getMessage();
+        }
+
+        return $this->connection;
+    }
+
 }
