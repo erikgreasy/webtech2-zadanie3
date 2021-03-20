821486eb394c31161284ceb28baa4bdfc83e3822
erik.masny <erik.masny@gmail.com>
2021-03-20 23:41:55 +0100

    Added functions to config.php

--- config.php
+++ config.php
@@ -4,10 +4,12 @@ ini_set('display_errors', 1);
 
 
 DEFINE( "BASE_URL", "http://localhost/webtech/zadanie3" );
-define( "ROUTING_PREFIX", 'webtech/zadanie2' );
+define( "ROUTING_PREFIX", 'webtech/zadanie3' );
 
 require __DIR__ . '/vendor/autoload.php';
 require_once 'inc/helper-router-functions.php';
+require_once 'inc/functions.php';
+
 
 // Start a Session
 if (!session_id()) @session_start();
