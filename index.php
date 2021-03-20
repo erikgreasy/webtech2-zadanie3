a3a63b8e45272e9a4993a4f13f5731aeff6153ef
erik.masny <erik.masny@gmail.com>
2021-03-20 23:42:15 +0100

    Added basic routes

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php 
 
 require_once 'config.php';
+require_once 'controller/AuthController.php';
 
+use Pecee\Http\Request;
+use Pecee\SimpleRouter\SimpleRouter as Router;
+
+// AUTH
+Router::get( ROUTING_PREFIX . '/' , 'AuthController@login');
+Router::get( ROUTING_PREFIX . '/register' , 'AuthController@register');
+
+
+// 404
+Router::error(function(Request $request, \Exception $exception) {
+    return view('core/404.php');
+});
+
+Router::start();
