a2748baea873bb9747dd3dacdb3bf971475183e8
erik.masny <erik.masny@gmail.com>
2021-03-25 21:04:29 +0100

    Added routes

--- index.php
+++ index.php
@@ -2,13 +2,20 @@
 
 require_once 'config.php';
 require_once 'controller/AuthController.php';
+require_once 'controller/DashboardController.php';
+
 
 use Pecee\Http\Request;
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
 // AUTH
-Router::get( ROUTING_PREFIX . '/' , 'AuthController@login');
+Router::get( ROUTING_PREFIX . '/' , 'DashboardController@show');
+Router::get( ROUTING_PREFIX . '/login' , 'AuthController@login');
+Router::post( ROUTING_PREFIX . '/login' , 'AuthController@handle_login');
 Router::get( ROUTING_PREFIX . '/register' , 'AuthController@register');
+Router::post( ROUTING_PREFIX . '/register' , 'AuthController@handleRegister');
+Router::post( ROUTING_PREFIX . '/logout' , 'AuthController@logout');
+
 
 
 // 404
