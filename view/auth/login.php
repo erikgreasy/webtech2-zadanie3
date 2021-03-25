22fbaabf8e417acd7e088a47d77a62027d480826
erik.masny <erik.masny@gmail.com>
2021-03-25 21:00:40 +0100

    Added content to login and register view

--- view/auth/login.php
+++ view/auth/login.php
@@ -1,7 +1,32 @@
 <?php include 'header.php' ?>
 
-<main>
-    login
+<main id="login">
+    <div class="container">
+        <form method="POST" action="<?= BASE_URL ?>/login">
+            <?php if( isset($errors) && !empty($errors) ): ?>
+                <div class="alert alert-danger" role="alert">
+                    <ul>
+                        <?php foreach( $errors as $error ): ?>
+                            <li>
+                                <?= $error ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+            <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Prihlásiť</button>
+        </form>
+    </div>
 </main>
 
 <?php include 'footer.php' ?>
--- view/auth/register.php
+++ view/auth/register.php
@@ -1,7 +1,53 @@
 <?php include 'header.php' ?>
 
-<main>
-    register
+<main id="register">
+    <div class="container">
+        <form method="POST" action="<?= BASE_URL ?>/register">
+            <?php if( isset($errors) && !empty($errors) ): ?>
+                <div class="alert alert-danger" role="alert">
+                    <ul>
+                        <?php foreach( $errors as $error ): ?>
+                            <li>
+                                <?= $error ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+            <div class="form-group">
+                <label for="name">Meno</label>
+                <input type="text" class="form-control" id="name" name="name" value="<?= isset( $_POST['name'] ) ? $_POST['name'] : ''  ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="surname">Priezvisko</label>
+                <input type="text" class="form-control" id="surname" name="surname" value="<?= isset( $_POST['surname'] ) ? $_POST['surname'] : ''  ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" value="<?= isset( $_POST['email'] ) ? $_POST['email'] : ''  ?>">
+            </div>
+
+            <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
+            </div>
+            
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            
+            <div class="form-group">
+                <label for="confirm_password">Zopakujte heslo</label>
+                <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Registrovať</button>
+        </form>
+    </div>
 </main>
 
 <?php include 'footer.php' ?>
