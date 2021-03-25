99c0729f207054503868c25ed5c8c3ddea477c9e
erik.masny <erik.masny@gmail.com>
2021-03-25 20:59:14 +0100

    Added bootstrap navbar with links

--- header.php
+++ header.php
@@ -16,5 +16,30 @@
 </head>
 <body>
 
-i am header
-    
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="<?= BASE_URL ?>">AUTH</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL ?>">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL ?>/login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= BASE_URL ?>/register">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" onclick="document.querySelector('#logout-form').submit()" href="#">Logout</a>
+
+                <form action="<?= BASE_URL ?>/logout" method="POST" class="d-none" id="logout-form">
+                    
+                </form>
+            </li>
+        
+        </ul>
+    </div>
+</nav>    
