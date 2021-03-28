3e126a10a856a2d4f53b1b5a02b5a11ceca6370c
erik.masny <erik.masny@gmail.com>
2021-03-28 14:44:07 +0200

    Created general config file for web to work. Moved env settings and other api keys related stuff to original config that is no longer tracked

---
+++ inc/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set('display_errors', 1);
+
+require 'config.php';
+require __DIR__ . '/../vendor/autoload.php';
+require_once 'inc/helper-router-functions.php';
+require_once 'inc/functions.php';
+  
+
+// AUTOLOAD
+spl_autoload_register(function ($class_name) {
+    include str_replace( '\\', '/', $class_name ) . '.php';
+});
+
+
+// Start a Session
+if (!session_id()) @session_start();
