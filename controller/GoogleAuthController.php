b263a4ab2583b6fbb557b72e7a46ada11500c102
erik.masny <erik.masny@gmail.com>
2021-03-28 17:42:07 +0200

    Created googleAuthController

---
+++ controller/GoogleAuthController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace controller;
+
+use repository\UserRepository;
+use repository\LoginRepository;
+use model\User;
+use model\Login;
+
+class GoogleAuthController {
+
+    private $userRepository;
+    private $loginRepository;
+
+    public function __construct() {
+        $this->userRepository = new UserRepository();
+        $this->loginRepository = new LoginRepository();
+    }
+
+
+    public function login() {
+        $user_id = $_SESSION['ga-uid'];
+        if( $user_id == null ) {
+            redirect( BASE_URL );
+        }
+        return view( 'auth/google/google-login.php',[
+            'user_id'   => $user_id
+        ] );
+    }
+
+
+    public function add() {
+        $user_id = $_SESSION['ga-uid'];
+        if( !$user_id ) {
+            redirect( BASE_URL );
+        }
+        
+        $websiteTitle = 'Webtech 2 - Zadanie 3';
+        $ga = new \PHPGangsta_GoogleAuthenticator();
+        $secret = $ga->createSecret();
+        $qrCodeUrl = $ga->getQRCodeGoogleUrl($websiteTitle, $secret);
+      
+        return view( 'auth/google/google-add.php', [
+            'user_id'   => $user_id,
+            'qr_url'    => $qrCodeUrl,
+            'secret'    => $secret,
+        ] );
+    }
+
+    public function handle_add() {
+        $secret = $_POST['google_secret'];
+        $user_id = $_POST['user_id'];
+
+        $this->userRepository->addSecret( $user_id, $secret );
+
+
+        redirect( 'google-login' );
+    }
+
+    public function check() {
+        $ga = new \PHPGangsta_GoogleAuthenticator();
+
+        $user_id = $_POST['user_id'];
+        $google_code = $_POST['google_code'];
+        
+        $user = $this->userRepository->getUser( $user_id );
+
+        $secret = $user->getGoogleAuth();
+
+        $checkResult = $ga->verifyCode($secret, $google_code, 2);    // 2 = 2*30sec clock tolerance
+
+        if ($checkResult) {
+            $login = new Login();
+            $login->setUserId( $user->getId() );
+            $login->setType( 'native' );
+            $this->loginRepository->add( $login );
+            $_SESSION['ga-uid'] = null;
+            login_user( $user->getId(), $login );
+
+        } else {
+            redirect( 'google-login' );
+        }
+
+        redirect( BASE_URL );
+    }
+
+    
+}
