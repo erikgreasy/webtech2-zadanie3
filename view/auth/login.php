5a1aec0a9bf5585def1d2261610101403a47b38a
erik.masny <erik.masny@gmail.com>
2021-03-28 13:16:22 +0200

    Added headings and changed layout

--- view/auth/login.php
+++ view/auth/login.php
@@ -2,30 +2,37 @@
 
 <main id="login">
     <div class="container">
-        <form method="POST" action="<?= BASE_URL ?>/login">
-            <?php if( isset($errors) && !empty($errors) ): ?>
-                <div class="alert alert-danger" role="alert">
-                    <ul>
-                        <?php foreach( $errors as $error ): ?>
-                            <li>
-                                <?= $error ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            <?php endif; ?>
+        <h1 class="display-3 text-center mb-3">Prihláste sa</h1>
+        <div class="row">
+            <div class="offset-md-3 col-md-6">
+                <form method="POST" action="<?= BASE_URL ?>/login">
+                    <?php if( isset($errors) && !empty($errors) ): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <ul>
+                                <?php foreach( $errors as $error ): ?>
+                                    <li>
+                                        <?= $error ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
 
-            <div class="form-group">
-                <label for="login">Login</label>
-                <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
+                    <div class="form-group">
+                        <label for="login">Login</label>
+                        <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Heslo</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary btn-block">Prihlásiť</button>
+                </form>
+                <a href="<?= $google_url ?>" class="btn btn-secondary btn-block">Google Login</a>
             </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            
-            <button type="submit" class="btn btn-primary">Prihlásiť</button>
-        </form>
+        </div>
+
     </div>
 </main>
 
--- view/auth/register.php
+++ view/auth/register.php
@@ -2,51 +2,57 @@
 
 <main id="register">
     <div class="container">
-        <form method="POST" action="<?= BASE_URL ?>/register">
-            <?php if( isset($errors) && !empty($errors) ): ?>
-                <div class="alert alert-danger" role="alert">
-                    <ul>
-                        <?php foreach( $errors as $error ): ?>
-                            <li>
-                                <?= $error ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                </div>
-            <?php endif; ?>
+        <h1 class="display-3 text-center mb-3">Registrujte sa</h1>
 
-            <div class="form-group">
-                <label for="name">Meno</label>
-                <input type="text" class="form-control" id="name" name="name" value="<?= isset( $_POST['name'] ) ? $_POST['name'] : ''  ?>">
-            </div>
+        <div class="row">
+            <div class="offset-md-3 col-md-6">
+                <form method="POST" action="<?= BASE_URL ?>/register">
+                    <?php if( isset($errors) && !empty($errors) ): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <ul>
+                                <?php foreach( $errors as $error ): ?>
+                                    <li>
+                                        <?= $error ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    <?php endif; ?>
 
-            <div class="form-group">
-                <label for="surname">Priezvisko</label>
-                <input type="text" class="form-control" id="surname" name="surname" value="<?= isset( $_POST['surname'] ) ? $_POST['surname'] : ''  ?>">
-            </div>
+                    <div class="form-group">
+                        <label for="name">Meno</label>
+                        <input type="text" class="form-control" id="name" name="name" value="<?= isset( $_POST['name'] ) ? $_POST['name'] : ''  ?>">
+                    </div>
 
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?= isset( $_POST['email'] ) ? $_POST['email'] : ''  ?>">
-            </div>
+                    <div class="form-group">
+                        <label for="surname">Priezvisko</label>
+                        <input type="text" class="form-control" id="surname" name="surname" value="<?= isset( $_POST['surname'] ) ? $_POST['surname'] : ''  ?>">
+                    </div>
 
-            <div class="form-group">
-                <label for="login">Login</label>
-                <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
-            </div>
-            
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            
-            <div class="form-group">
-                <label for="confirm_password">Zopakujte heslo</label>
-                <input type="password" class="form-control" id="confirm_password" name="confirm_password">
-            </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" value="<?= isset( $_POST['email'] ) ? $_POST['email'] : ''  ?>">
+                    </div>
 
-            <button type="submit" class="btn btn-primary">Registrovať</button>
-        </form>
+                    <div class="form-group">
+                        <label for="login">Login</label>
+                        <input type="text" class="form-control" id="login" name="login" value="<?= isset( $_POST['login'] ) ? $_POST['login'] : ''  ?>">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="password">Heslo</label>
+                        <input type="password" class="form-control" id="password" name="password">
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="confirm_password">Zopakujte heslo</label>
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-block">Registrovať</button>
+                </form>
+            </div>
+        </div>
     </div>
 </main>
 
