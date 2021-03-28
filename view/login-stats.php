aea8e9e8b204c584db1701ffed26fe389c3c55db
erik.masny <erik.masny@gmail.com>
2021-03-28 17:43:24 +0200

    Added all logins stats to view

--- view/login-stats.php
+++ view/login-stats.php
@@ -2,25 +2,46 @@
 
 <main>
     <div class="container">
-        <h2>Moje prihlásenia</h2>
-        <table class="table">
-                
-            <tr>
-                <th>Typ prihlásenia</th>
-                <th>Dátum</th>
-            </tr>
-            <?php foreach( $user_logins as $login ): ?>
+        <div>
+            <h2>Moje prihlásenia</h2>
+            <table class="table">
+                    
                 <tr>
-                    <td>
-                        <?php print_r($login->getType()) ?>
-                    </td>
-                    <td>
-                        <?php print_r($login->getTime()) ?>
-                    </td>
+                    <th>Typ prihlásenia</th>
+                    <th>Dátum</th>
                 </tr>
-            <?php endforeach; ?>
+                <?php foreach( $user_logins as $login ): ?>
+                    <tr>
+                        <td>
+                            <?php print_r($login->getType()) ?>
+                        </td>
+                        <td>
+                            <?php print_r($login->getTime()) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
 
-        </table>
+            </table>
+        </div>
+        <div>
+            <h2>Štatistiky</h2>
+            <table class="table">
+                <tr>
+                    <th>Typ prihlásenia</th>
+                    <th>Počet prihlásení</th>
+                </tr>
+
+                <?php foreach( $login_stats as $login ): ?>
+
+                    <tr>
+                        <td><?= $login->type ?></td>
+                        <td><?= $login->count ?></td>
+
+                    </tr>
+                <?php endforeach; ?>
+
+            </table>
+        </div>
     </div>
 </main>
 
